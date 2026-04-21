e9ebfcd59df784a84fa6e87197404efa3eb67626
SmitBZ <smit15zurita@hotmail.com>
2026-04-21 10:09:51 -0500

    se actualizo header

--- includes/components/header.php
+++ includes/components/header.php
@@ -96,7 +96,7 @@
                             Configuración
                         </a>
                         <hr class="my-1 border-gray-100">
-                        <a href="../controller/ControllerLogout.php" class="flex items-center gap-3 px-4 py-2 text-sm text-red-500 hover:bg-red-50 transition font-medium">
+                        <a href="../../src/Controllers/salir_conexion.php" class="flex items-center gap-3 px-4 py-2 text-sm text-red-500 hover:bg-red-50 transition font-medium">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                             </svg>
